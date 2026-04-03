ced98a42d8d07ef56f5435f278ff013eb12caa87
Nazim6269 <nazimdev10022001@gmail.com>
2026-04-03 11:07:50 +0600

    nazim two commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import './App.css'
 
 function App() {
   return (
-  <h2>This is First Line</h2>
+ <div>
+   <h2>This is First Line</h2>
+   <h3>Hi there i from, nazim two branch</h3>
+ </div>
+  
   )
 }
 
