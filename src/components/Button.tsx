f85c48d69f20db08a27973692e7df51650162c2d
Nazim6269 <nazimdev10022001@gmail.com>
2026-04-04 12:45:17 +0600

    add a counter

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Button = () => {
+  const [count, setCount] = useState(0)
   return (
-    <div>count</div>
+    <div>{count}
+      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+      <button onClick={() => setCount((prev) => prev - 1)}>Decrement</button>
+    </div>
   )
 }
 
