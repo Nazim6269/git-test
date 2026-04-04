aaa639fa1594f4d1e3309b124736d2ab10e76f2a
Nazim6269 <nazimdev10022001@gmail.com>
2026-04-04 12:45:17 +0600

    add a counter

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Button = () => {
+<<<<<<< HEAD
   const [count, setCount] = React.useState(0)
   return (
     <div>{count}
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <p>This line is added by nazimTwo</p>
+=======
+  const [count, setCount] = useState(0)
+  return (
+    <div>{count}
+      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+      <button onClick={() => setCount((prev) => prev - 1)}>Decrement</button>
+>>>>>>> f85c48d (add a counter)
     </div>
   )
 }
