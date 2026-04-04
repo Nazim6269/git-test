4b00b8f7b0aa47ef5bfcf763dad54640cf16fbeb
Nazim6269 <nazimdev10022001@gmail.com>
2026-04-04 12:45:42 +0600

    Revert "add a counter"
    This reverts commit f85c48d69f20db08a27973692e7df51650162c2d.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const Button = () => {
-  const [count, setCount] = useState(0)
   return (
-    <div>{count}
-      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
-      <button onClick={() => setCount((prev) => prev - 1)}>Decrement</button>
-    </div>
+    <div>count</div>
   )
 }
 
