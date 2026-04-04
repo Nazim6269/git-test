ba6fa9336f98b8f99252b9529439427f37b8f827
Nazim6269 <nazimdev10022001@gmail.com>
2026-04-04 14:18:25 +0600

    add another para

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,6 +6,7 @@ const Button = () => {
     <div>{count}
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <p>Add a paragraph to practice cherry pick</p>
+      <p>Add an another paragraph  for checking cherry pick</p>
     </div>
   )
 }
