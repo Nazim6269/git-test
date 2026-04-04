899999e7ac7cca1e4c1255321baac99833447be0
Nazim6269 <nazimdev10022001@gmail.com>
2026-04-04 14:04:06 +0600

    commited

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
 const Button = () => {
+  const [count, setCount] = React.useState(0)
   return (
-    <div>count</div>
+    <div>{count}
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
   )
 }
 
