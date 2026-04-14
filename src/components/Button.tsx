e192e988943d408d878d2788fa26a40f63623639
Nazim6269 <nazimdev100220001@gmail.com>
2026-04-14 09:50:36 +0600

    add a line by nazim

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,6 +5,7 @@ const Button = () => {
   return (
     <div>{count}
       <button onClick={() => setCount(count + 1)}>Increment</button>
+      <p>This line is added by nazim</p>
     </div>
   )
 }
