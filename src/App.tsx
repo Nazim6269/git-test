53b7a2814c0c4363053bd679d0f4bc80f609a312
Nazim6269 <nazimdev10022001@gmail.com>
2026-04-21 12:10:43 +0600

    revert

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,7 @@ function App() {
   return (
  <div>
    <h2>This is First Line</h2>
-   <p>I from nazim One</p>
-    <p>This line is written by nazim One</p>
+   <h3>Hi there i from, nazim two branch</h3>
  </div>
   
   )
