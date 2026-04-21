bcdfe04d3953914eebc8d8a97b627ea3e36c2649
Nazim6269 <nazimdev10022001@gmail.com>
2026-04-21 11:24:20 +0600

    add: add tex

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,10 @@
-
 const Header = () => {
   return (
     <div>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, nulla
       labore vel porro consectetur quos quibusdam nesciunt quidem! Placeat
       porro, magnam dicta enim sit laborum quis a quos unde explicabo.
-     
+      <p>I am from nazim Two branch</p>
     </div>
   );
 };
