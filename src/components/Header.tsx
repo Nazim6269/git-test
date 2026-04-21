1f6d4b36e46aa2bd453a55a3b2ea4f3f7477b1ca
Nazim6269 <nazimdev10022001@gmail.com>
2026-04-21 11:31:35 +0600

    refactor: remove shub text from nazim

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,6 @@ const Header = () => {
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, nulla
       labore vel porro consectetur quos quibusdam nesciunt quidem! Placeat
       porro, magnam dicta enim sit laborum quis a quos unde explicabo.
-      {/* This is a comment */}
-      This is pre commit state
-      {/* this is a comment */}
-      This is post commit state
-      <p>This line is written by nazim</p>
     </div>
   );
 };
